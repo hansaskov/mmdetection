59bb8b3664dcb53faeb7da849271a9b93d390414
hansaskov <hansaskov1999@gmail.com>
2023-04-27 17:51:53 +0200

    training working - wrong validation output

--- configs/ic_bin/mask_rcnn.py
+++ configs/ic_bin/mask_rcnn.py
@@ -1,5 +1,5 @@
 # The new config inherits a base config to highlight the necessary modification
-_base_ = '../mask_rcnn/mask-rcnn_r50-caffe_fpn_ms-poly-1x_coco.py'
+_base_ = '../mask_rcnn/mask-rcnn_r50_fpn_1x_coco.py'
 
 # We also need to change the num_classes in head to match the dataset's annotation
 model = dict(
@@ -9,9 +9,9 @@ model = dict(
 # Modify dataset related settings
 data_root = 'data/ic_bin/'
 metainfo = {
-    'classes': ('1','2' ),
+    'classes': ('1','2'),
     'palette': [
-        (220, 20, 60),
+        [(220, 20, 60), (119, 11, 32)],
     ]
 }
 train_dataloader = dict(
@@ -34,4 +34,4 @@ val_evaluator = dict(ann_file=data_root + 'val/000000/scene_gt_coco.json')
 test_evaluator = val_evaluator
 
 # We can use the pre-trained Mask RCNN model to obtain higher performance
-load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
+#load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
